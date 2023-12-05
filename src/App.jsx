a2dd467bf1d0a94b886df99ac2e1f2d72feda3c9
Heather Lockett <heatherlockett@Heathers-MacBook-Pro.local>
2023-12-05 13:17:30 -0500

    yeah

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-
+import {puppyList} from './data.js'
 import './App.css'
+import { useState } from 'react'
+
+
+
+
 
 function App() {
 
+  const [puppies, setPuppies] = useState(puppyList);
 
+  console.log("puppyList: ", puppyList);
+
+  return (
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    </div>
+  );
 }
 
 export default App
